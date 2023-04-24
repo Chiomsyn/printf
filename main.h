15a51d9a4d5b8154459cf7e6fc743b9e55f87145
Chioma <mirianangelk@gmail.com>
2023-04-24 04:43:47 -0700

    rectifying betty

--- main.h
+++ main.h
@@ -21,7 +21,7 @@ typedef struct print_type
 
 int _printf(const char *format, ...);
 int print_stream(char *buf, unsigned int count);
-int (*get_print_func(const char *n, int index))(va_list, char*, unsigned int)
+int (*get_print_func(const char *n, int index))(va_list, char*, unsigned int);
 int print_chr(va_list ptr, char *tmp, unsigned int count);
 
 #endif
