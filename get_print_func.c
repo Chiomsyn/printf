d87f59f9c16654abf424ee20a6c16c87d651e760
Chioma <mirianangelk@gmail.com>
2023-04-24 17:33:25 -0700

    fixed the print string bug

--- get_print_func.c
+++ get_print_func.c
@@ -13,13 +13,24 @@ int (*get_print_func(const char *n, int index))(va_list, char*, unsigned int)
 		{" %", print_percnt}, {"d", print_int}, {"s", print_str},
 		{"l", print_percnt}, {"h", print_percnt}, {NULL, NULL},
 	};
-	int i = 0;
+	int i = 0, j = 0, first_index;
 
+	first_index = index;
 	while (pt[i].sym)
 	{
 		if (n[index] == pt[i].sym[0])
-			break;
-		i++;
+		{
+			if (pt[i].sym[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
 
 	return (pt[i].f);
