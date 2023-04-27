ce19719fba020a97510e4eeec4cb8df32355c936
Akah5David <davidakah1999@gmail.com>
2023-04-27 12:07:36 -0700

    prints formats

--- main.h
+++ main.h
@@ -31,5 +31,15 @@ int c_print_func(const char *s, int index);
 
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, in
 
 #endif
